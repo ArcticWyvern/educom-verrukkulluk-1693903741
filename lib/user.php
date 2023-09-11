0f6214af2a0c8b29a2a2f9aba2509d29287fa39b
ArcticWyvern <stan2.hillen@gmail,com>
2023-09-11 13:22:35 +0200

    update user (#6)

--- lib/user.php
+++ lib/user.php
@@ -10,7 +10,7 @@ class User {
   
     public function selectUser($user_id) {
 
-        $sql = "select * from user where id = $user_id";
+        $sql = "SELECT * FROM user WHERE id = $user_id";
         
         $result = mysqli_query($this->connection, $sql);
         $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
