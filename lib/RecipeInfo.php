95120ee19409c14a2665b02f48f13d1a463e6335
ArcticWyvern <stan2.hillen@gmail,com>
2023-09-12 11:29:00 +0200

    update selectinfo (#8)

--- lib/RecipeInfo.php
+++ lib/RecipeInfo.php
@@ -8,57 +8,64 @@ class RecipeInfo {
         $this->connection = $connection;
     }
   
-    public function selectInfo($info_id) {
+    public function selectInfo($recipe_id, $record_type) {
 
-        $sql = "SELECT id, record_type, recipe_id, user_id, date FROM recipeinfo WHERE id = $info_id";
+        $sql = "SELECT id, record_type, recipe_id, user_id, date FROM recipeinfo WHERE recipe_id = $recipe_id";
         
         $result = mysqli_query($this->connection, $sql);
-        $recipeinfo = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $infoarr = $this->resultToArray($result);
 
-        switch ($recipeinfo[1]) {
-            case "O": //comment
-                $queryone = "SELECT commenter_id, comment FROM recipeinfo WHERE id = $info_id";
-                $commentfetch = mysqli_query($this->connection, $queryone);
-                $comment = mysqli_fetch_array($commentfetch, MYSQLI_ASSOC);
-                $querytwo = "SELECT * FROM user WHERE id = $comment[0]";
-                $user = mysqli_query($this->connection, $querytwo);
-                $append = mysqli_fetch_array($user, MYSQLI_ASSOC);
 
-                foreach ($append as $value) {
-                    array_push($recipeinfo, $value);
+
+        foreach ($infoarr as $recipeinfo) 
+            {
+                if($recipeinfo["record_type"] == $record_type) {
+                    switch ($recipeinfo["record_type"]) {
+                    case "O": //comment
+                        $queryone = "SELECT commenter_id, comment FROM recipeinfo WHERE id = $recipeinfo[0]";
+                        $commentfetch = mysqli_query($this->connection, $queryone);
+                        $comment = mysqli_fetch_array($commentfetch, MYSQLI_ASSOC);
+                        $querytwo = "SELECT * FROM user WHERE id = $comment[0]";
+                        $user = mysqli_query($this->connection, $querytwo);
+                        $append = mysqli_fetch_array($user, MYSQLI_ASSOC);
+
+                        foreach ($append as $value) {
+                            array_push($recipeinfo, $value);
+                        }
+
+                    case "F": //favourite
+                        $queryone = "SELECT fav_id FROM recipeinfo WHERE id = $recipeinfo[0]";
+                        $favfetch = mysqli_query($this->connection, $queryone);
+                        $fav = mysqli_fetch_array($favfetch, MYSQLI_ASSOC);
+                        $querytwo = "SELECT * FROM user WHERE id = $fav[0]";
+                        $user = mysqli_query($this->connection, $querytwo);
+                        $append = mysqli_fetch_array($user, MYSQLI_ASSOC);
+
+                        foreach ($append as $value) {
+                            array_push($recipeinfo, $value);
+                        }
+
+                    case "B": //recipe step
+                        $queryone = "SELECT step, step_text FROM recipeinfo WHERE id = $recipeinfo[0]";
+                        $stepfetch = mysqli_query($this->connection, $queryone);
+                        $append = mysqli_fetch_array($stepfetch, MYSQLI_ASSOC);
+
+                        foreach ($append as $value) {
+                            array_push($recipeinfo, $value);
+                        }
+
+                    case "W": //rating
+                        $queryone = "SELECT rating FROM recipeinfo WHERE id = $recipeinfo[0]";
+                        $ratingfetch = mysqli_query($this->connection, $queryone);
+                        $append = mysqli_fetch_array($ratingfetch, MYSQLI_ASSOC);
+                    
+                        foreach ($append as $value) {
+                            array_push($recipeinfo, $value);
+                        }
                 }
-
-            case "F": //favourite
-                $queryone = "SELECT fav_id FROM recipeinfo WHERE id = $info_id";
-                $favfetch = mysqli_query($this->connection, $queryone);
-                $fav = mysqli_fetch_array($favfetch, MYSQLI_ASSOC);
-                $querytwo = "SELECT * FROM user WHERE id = $fav[0]";
-                $user = mysqli_query($this->connection, $querytwo);
-                $append = mysqli_fetch_array($user, MYSQLI_ASSOC);
-
-                foreach ($append as $value) {
-                    array_push($recipeinfo, $value);
-                }
-
-            case "B": //recipe step
-                $queryone = "SELECT step, step_text FROM recipeinfo WHERE id = $info_id";
-                $stepfetch = mysqli_query($this->connection, $queryone);
-                $append = mysqli_fetch_array($stepfetch, MYSQLI_ASSOC);
-
-                foreach ($append as $value) {
-                    array_push($recipeinfo, $value);
-                }
-
-            case "W": //rating
-                $queryone = "SELECT rating FROM recipeinfo WHERE id = $info_id";
-                $ratingfetch = mysqli_query($this->connection, $queryone);
-                $append = mysqli_fetch_array($ratingfetch, MYSQLI_ASSOC);
-                
-                foreach ($append as $value) {
-                    array_push($recipeinfo, $value);
-                }
+            }
         }
-        return($recipeinfo);
+        return($infoarr);
 
     }
 
@@ -81,5 +88,14 @@ class RecipeInfo {
                 echo $sql . "<br>" . $e->getMessage();
             }
     }
+
+    private function resultToArray($result) {
+        $rows = array();
+        while($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+
 
 }
