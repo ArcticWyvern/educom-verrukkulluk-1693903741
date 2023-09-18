d3bc80fa0bff1284129e25b1673c8cf6982d2f95
ArcticWyvern <stan2.hillen@gmail,com>
2023-09-18 09:42:46 +0200

    ShoppingList (#12)

--- lib/ShoppingList.php
+++ lib/ShoppingList.php
@@ -8,59 +8,89 @@ class ShoppingList {
         $this->connection = $connection;
     }
 
-    private function selectUser($recipe_id) {
-        $sql = "SELECT user_id FROM recipe WHERE id = $recipe_id";
+    
+    public function selectShoppingList($user_id) {
+        $sql = "SELECT * FROM shoppinglist WHERE user_id = $user_id";
+        $listFetch = mysqli_query($this->connection, $sql);
+
+        return $this->resultToArray($listFetch);
+    }
+
+
+    public function addIngredients($recipe_id, $user_id) {
+        $ingredients = $this->selectIngredient($recipe_id); 
+        
+        foreach ($ingredients as $ingredient) {
+            if (is_array($this->articleOnList($ingredient["article_id"], $user_id))) {
+                $listItem = $this->articleOnList($ingredient["article_id"], $user_id);
+                $amountTotal = $ingredient["amount"] + $listItem["amount"];
+                $item_id = $listItem["id"];
+
+                $updateQuery = "UPDATE shoppinglist SET amount = $amountTotal WHERE id = $item_id";
+
+                mysqli_query($this->connection, $updateQuery);
+            } else {
+
+                $ingr_id = $ingredient["id"];
+                $amount = $ingredient["amount"];
+                $addIngrToList = "INSERT INTO shoppinglist (ingredient_id, user_id, amount) VALUES ($ingr_id, $user_id, $amount)";
+                mysqli_query($this->connection, $addIngrToList);
+            }
+        } 
+    }
+
+
+
+    private function articleOnList($article_id, $user_id) {
+        $sql = "SELECT * FROM shoppinglist WHERE user_id = $user_id";
+        $listfetch = mysqli_query($this->connection, $sql);
+        $articlesonlist = $this->resultToArray($listfetch);
+
+        foreach ($articlesonlist as $article) {
+            $ingr_id = $article ["ingredient_id"];
+
+            $ingrQuery = "SELECT article_id FROM ingredients WHERE id = $ingr_id";
+            
+            $ingrFetch = mysqli_query($this->connection, $ingrQuery);
+            $ingr = mysqli_fetch_array($ingrFetch, MYSQLI_ASSOC);
+
+            $article_id = $ingr["article_id"];
+
+            $artQuery = "SELECT * FROM article WHERE id = $article_id";
+        
+            $artFetch = mysqli_query($this->connection, $artQuery);
+            $art = mysqli_fetch_array($artFetch, MYSQLI_ASSOC);
+
+            if ($art["id"] == $article_id) {
+                return $article;
+            }
+        }   //room for improvement
+
+        return false;
+
+    }
+
+    private function selectUser($list_id) {
+        $sql = "SELECT user_id FROM shoppinglist WHERE id = $list_id";
         
         $result = mysqli_query($this->connection, $sql);
-        $recipe = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-        $user_id = $recipe[0]; 
+        $list = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        $user_id = $list[0]; 
         $user = new User($this->connection);
 
         return($user->selectUser($user_id));
     }
 
     private function selectIngredient($recipe_id) {
-        $ingrfetch = new Ingredient($this->connection);
+        $ingrFetch = new Ingredient($this->connection);
         //$ingr = mysqli_fetch_array($ingrfetch, MYSQLI_ASSOC);
 
-        return($ingrfetch->selectIngredient($recipe_id)); 
+        return($ingrFetch->selectIngredient($recipe_id)); 
         //returns nested array with each entry of the array having an array with the ingredient data in it
     }
 
-    function addIngredients($recipe_id, $user_id) {
-        $ingredients = $this->selectIngredient($recipe_id); 
-        
-        
-        foreach ($ingredients as $ingredient) {
-            if (is_array($this->articleOnList($ingredient["article_id"], $user_id))) {
-                foreach ($list as &$entry){
-                    $entry['amount'] += $ingredient['amount'];
-                }
-            } else {
-                array_push($list, $ingredient);
-            }
-        }
-    }
-
-
-
-    private function articleOnList($article_id, $user_id) {
-        $groceries = $this->retrieveGroceries($user_id); //placeholder
-
-        foreach ($groceries as $art) {
-            if ($art == $article_id){
-                return $art;
-            }
-        }
-
-        return false;
-
-    }
-
-    private function retrieveGroceries($user_id) {
-        return []; //placeholder
-    }
 
     private function resultToArray($result) {
         $rows = array();
